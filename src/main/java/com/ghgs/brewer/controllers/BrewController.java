bf645ca81facfc76ecdab75ae0ab5b46625b5201
George Silva <georgehg@hotmail.com>
2017-05-24 00:02:52 -0300

    Thymeleaf configuration

--- src/main/java/com/ghgs/brewer/controllers/BrewController.java
+++ src/main/java/com/ghgs/brewer/controllers/BrewController.java
@@ -1,8 +1,20 @@
 package com.ghgs.brewer.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BrewController {
+	
+	@RequestMapping("brews/new")
+	public String add() {
+		return "brew/BrewManagement";
+	}
+	
+	@RequestMapping(value = "brews/new", method = RequestMethod.POST)
+	public void cadastrar() {
+		
+	}
 
 }
---
+++ src/main/java/com/ghgs/brewer/model/Brew.java
@@ -0,0 +1,7 @@
+package com.ghgs.brewer.model;
+
+public class Brew {
+	
+	
+
+}
